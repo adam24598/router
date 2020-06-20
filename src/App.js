aa27b9d52d977d81c1ebe7dfe641978a3ec261c7
adam24598 <64758097+adam24598@users.noreply.github.com>
2020-06-20 09:14:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Redirect,Link,BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Category from './component/Category';
+import Products from './component/Products';
+import {Login,fakeAuth} from './component/Login'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    function Home(){
+      return(
+        <h1>Home</h1>
+      );
+
+    }
+    const PrivateRoute = ({component: Component, authed, ...rest}) => {
+      return (
+        <Route
+          {...rest}
+          render={(props) => authed === true
+            ? <Component {...props} />
+            : <Redirect to={{pathname: '/login', state: {from: props.location}}} />} />
+      )
+    }
+    return (
+      <Router>
+      <div>
+        <nav className="navbar navbar-light">
+          <ul className="nav navbar-nav">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/category">Category</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/Login">Login</Link></li>
+
+          </ul>
+       </nav>
+
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/category" component={Category}/>
+       <Route path="/products" component={Products}/>
+       <PrivateRoute authed={fakeAuth.isAuthenticated} path='/products' component = {Products} />
+
+    </Switch>
+
     </div>
-  );
-}
+    </Router>
+    );
+  }
 
 export default App;
